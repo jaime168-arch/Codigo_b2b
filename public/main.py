5d6eeb17e30d4cd445f99d65723865c1594bd425
jaime_r_pereira <jaime_r_pereira@estudante.sesisenai.org.br>
2026-06-17 09:38:19 -0300

    add- codigo quase completo

--- public/main.py
+++ public/main.py
@@ -37,5 +37,32 @@ def buscar_contatos():
         logging.error(f"Erro ao buscar dados no Supabase: {e}")
         return[]
 
-    def enviar_mensagem_zapi(nome, telefone):    
+    def enviar_mensagem_zapi(nome, telefone):   
+        """Envia a mensagem exata exigida pelo desafio via Z-API."""
+        url =  f"https://api.z-api.io/instances/{ZAPI_INSTANCE}/token/{ZAPI_TOKEN}/send-text"
 
+        payload = {
+            "phone": telefone,
+            "mensage": f"Olá, {nome} tudo bem com você?"
+        }
+
+        headers = {
+            "Content-Type": "application/json"
+        }
+
+        try:
+            response = request.post(url, json=payload, headers=headers)
+            if response.status_bytes or response.status_code in [200,201]:
+                logging.info(f"Mensagem enviada com sucesso ára {nome} ({telefone})")
+                else:
+                    logging.warning(f"Falha ao enviar para {nome}. Status: {response.status_code} - {response_text}")
+            except request.exceptions.RequesytException as e:
+                logging.error(f"Erro na requisição HTTP para a Z-API: {e}")
+
+def main():
+    logging.info("Iniciando o fluxo b2bflow...")
+    contatos = buscar_contatos()
+
+    if not contatos:
+        logging.warning("Nenhum contato encontrado ou erro na busca.")
+        return
